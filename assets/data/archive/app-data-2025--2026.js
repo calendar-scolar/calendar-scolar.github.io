c91f3dd832a67234311ea6d5212b48d33f558ef5
RePierre <petru.rebeja@gmail.com>
2026-07-05 14:51:22 +0300

    Added data for school year 2026-2027

---
+++ assets/data/archive/app-data-2025--2026.js
@@ -0,0 +1,130 @@
+export const SchoolYear = {
+  start: "2025-09-08",
+  end: {
+    all: "2026-06-19",
+    gymnasium: "2026-06-05",
+    highSchool: "2026-06-12",
+  },
+  calendar: [
+    {
+      type: "module",
+      start: "2025-09-08",
+      end: "2025-10-24",
+      name: "Modulul 1",
+      color: "#3498db",
+    },
+    {
+      type: "vacation",
+      start: "2025-10-27",
+      end: "2025-10-31",
+      name: "Vacanța de toamnă",
+      color: "#e74c3c",
+    },
+    {
+      type: "module",
+      start: "2025-11-03",
+      end: "2025-12-19",
+      name: "Modulul 2",
+      color: "#2ecc71",
+    },
+    {
+      type: "vacation",
+      start: "2025-12-22",
+      end: "2026-01-07",
+      name: "Vacanța de iarnă",
+      color: "#f39c12",
+    },
+    {
+      type: "module",
+      start: "2026-01-08",
+      end: "2026-02-08",
+      name: "Modulul 3",
+      unitCodes: "BN, CJ, TM",
+      color: "#7f8c8d",
+    },
+    {
+      type: "vacation",
+      start: "2026-02-09",
+      end: "2026-02-15",
+      name: "Vacanța de schi",
+      unitCodes: "BN, CJ, TM",
+      color: "#454de1",
+    },
+    {
+      type: "module",
+      start: "2026-02-16",
+      end: "2026-04-03",
+      name: "Modulul 4",
+      unitCodes: "BN, CJ, TM",
+      color: "#55efc4",
+    },
+    {
+      type: "module",
+      start: "2026-01-08",
+      end: "2026-02-13",
+      name: "Modulul 3",
+      unitCodes:
+        "AR, AG, BH, BV, B, BZ, CL, CS, DB, DJ, GJ, HD, IL, IS, IF, MH, OT, PH, SJ, TR, TL, VL",
+      color: "#7f8c8d",
+    },
+    {
+      type: "vacation",
+      start: "2026-02-16",
+      end: "2026-02-20",
+      name: "Vacanța de schi",
+      unitCodes:
+        "AR, AG, BH, BV, B, BZ, CL, CS, DB, DJ, GJ, HD, IL, IS, IF, MH, OT, PH, SJ, TR, TL, VL",
+      color: "#ff3f35",
+    },
+    {
+      type: "module",
+      start: "2026-02-23",
+      end: "2026-04-03",
+      name: "Modulul 4",
+      unitCodes:
+        "AR, AG, BH, BV, B, BZ, CL, CS, DB, DJ, GJ, HD, IL, IS, IF, MH, OT, PH, SJ, TR, TL, VL",
+      color: "#55efc4",
+    },
+    {
+      type: "module",
+      start: "2026-01-08",
+      end: "2026-02-22",
+      name: "Modulul 3",
+      unitCodes:
+        "AB, BC, BT, BR, CT, CV, GL, GR, HG, HR, MM, MS, NT, SM, SB, SV, VS, VN",
+      color: "#7f8c8d",
+    },
+    {
+      type: "vacation",
+      start: "2026-02-23",
+      end: "2026-03-01",
+      name: "Vacanța de schi",
+      unitCodes:
+        "AB, BC, BT, BR, CT, CV, GL, GR, HG, HR, MM, MS, NT, SM, SB, SV, VS, VN",
+      color: "#ffc930",
+    },
+    {
+      type: "module",
+      start: "2026-03-02",
+      end: "2026-04-03",
+      name: "Modulul 4",
+      unitCodes:
+        "AB, BC, BT, BR, CT, CV, GL, GR, HG, HR, MM, MS, NT, SM, SB, SV, VS, VN",
+      color: "#55efc4",
+    },
+    {
+      type: "vacation",
+      start: "2026-04-06",
+      end: "2026-04-14",
+      name: "Vacanța de primăvară",
+      color: "#e1bc29",
+    },
+    {
+      type: "module",
+      start: "2026-04-15",
+      end: "2026-06-19",
+      name: "Modulul 5",
+      color: "#8e44ad",
+    },
+  ],
+};
